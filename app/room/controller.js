5e35e2e2830a9be3c1f929d0cd77dfd7020fc321
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-22 12:34:19 -0500

    Finished project

--- app/room/controller.js
+++ app/room/controller.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 import moment from 'moment';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
+  init() {
+    this._super(...arguments);
+    this.get(`waitAFewSeconds`).perform();
+  },
+
+  waitAFewSeconds: task(function * () {
+    while(true) {
+      yield timeout(1000);
+      this.scroll();
+      console.log(`polling`);
+      this.store.findAll(`message`, { include: `chatter` });
+    }
+  }),
+
   moment: Ember.inject.service(),
   newContent: ``,
 
@@ -11,5 +26,10 @@ export default Ember.Controller.extend({
     msg.save().then(() => {
       this.set(`newContent`, ``);
     });
+  },
+  scroll() {
+    var x = $(`.chatroom-box`)
+    var scrollheight = x.height;
+    x.scrollTop(100000000);
   },
 });
